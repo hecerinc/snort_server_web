5a4da1c3c500da55ce510bac3f9164499c445db8
psanchezp <psanchezp94@gmail.com>
2016-09-29 11:58:49 -0500

    Fixed untracked files

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import json
 import os
 import mysql.connector
 # from os.path import join, dirname
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# dotenv_path = join(dirname(__file__), '.env')
-# load_dotenv(dotenv_path)
+dotenv_path = './.env'
+load_dotenv(dotenv_path)
 
 app = Flask (__name__)
 #app.debug = True
